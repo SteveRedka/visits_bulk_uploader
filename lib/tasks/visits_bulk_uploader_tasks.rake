01dd9722f2ddd9a3d38e1094c8c9e4e386bdbe81
Steve Redka <cogitofallacy@gmail.com>
2018-11-02 14:11:44 +0300

    feat: add filters

--- lib/tasks/visits_bulk_uploader_tasks.rake
+++ lib/tasks/visits_bulk_uploader_tasks.rake
@@ -1,20 +1,41 @@
 require 'visits_bulk_uploader/uploader'
 require 'optionparser'
+require 'byebug'
 
 namespace :visits_bulk_uploader do
   desc 'Import Visits from xml file'
   task :import do
-    options = {}
+    filters = {}
+    path_to_file = ''
 
     opts = OptionParser.new
     opts.banner = 'Usage: visits_bulk_uploader:import -- --file {path_to_file}'
-    opts.on('--file FILE') do |file|
-      options[:path_to_file] = file
+
+    opts.on('-f', '--file [FILE]', 'Target file location') do |file|
+      path_to_file = file
+    end
+
+    opts.on('--min_sum [DECIMAL]', 'Minimum sum') do |val|
+      filters[:min_sum] = val.to_f
+    end
+
+    opts.on('--max_sum [DECIMAL]', 'Maximum sum') do |val|
+      filters[:max_sum] = val.to_f
+    end
+
+    opts.on('--min_end_at [DATETIME]', 'Earliest time of visit end') do |val|
+      filters[:min_end_at] = val
+    end
+
+    opts.on('--max_end_at [DATETIME]', 'Latest time of visit end') do |val|
+      filters[:max_end_at] = val
     end
 
     args = opts.order!(ARGV) {}
     opts.parse!(args)
 
-    VisitsBulkUploader::Uploader.new(options[:path_to_file]).call
+    raise OptionParser::MissingArgument if path_to_file.empty?
+
+    VisitsBulkUploader::Uploader.new(path_to_file, filters).call
   end
 end
--- spec/lib/tasks/visits_bulk_uploader_tasks_spec.rb
+++ spec/lib/tasks/visits_bulk_uploader_tasks_spec.rb
@@ -1,16 +1,51 @@
+# Supress ARGV warnings
+$VERBOSE = nil
+
 require 'spec_helper'
 require 'rake'
 load File.expand_path("#{RSPEC_ROOT}/../lib/tasks/visits_bulk_uploader_tasks.rake")
 
 describe 'visits_bulk_uploader_tasks' do
+  let :run_rake_task do
+    Rake::Task['visits_bulk_uploader:import'].reenable
+    Rake::Task['visits_bulk_uploader:import'].invoke
+  end
+
   sample_location = "#{RSPEC_ROOT}/fixtures/sample.xml"
   describe ':import' do
     it 'works' do
       # Emulate task call with arguments
       ARGV = ['__FILE__', '--', '--file', sample_location]
-      expect do
-        Rake::Task['visits_bulk_uploader:import'].invoke
-      end.to change { Visit.count }.by(2)
+      expect { run_rake_task }.to change { Visit.count }.by(2)
+    end
+
+    it 'raises an error if no location is given' do
+      ARGV = ['__FILE__']
+      expect { run_rake_task }.to raise_error(OptionParser::MissingArgument)
+    end
+
+    describe 'filters' do
+      it 'processes min_sum' do
+        ARGV = ['__FILE__', '--', '--file', sample_location, '--min_sum', '100']
+        expect { run_rake_task }.to change { Visit.count }.by(1)
+      end
+
+      it 'processes max_sum' do
+        ARGV = ['__FILE__', '--', '--file', sample_location, '--max_sum', '100']
+        expect { run_rake_task }.to change { Visit.count }.by(1)
+      end
+
+      it 'processes min_end_at' do
+        ARGV = ['__FILE__', '--', '--file', sample_location,
+                '--min_end_at', '2018-10-26-12:00']
+        expect { run_rake_task }.to change { Visit.count }.by(1)
+      end
+
+      it 'processes max_end_at' do
+        ARGV = ['__FILE__', '--', '--file', sample_location,
+                '--max_end_at', '2018-10-26-12:00']
+        expect { run_rake_task }.to change { Visit.count }.by(1)
+      end
     end
   end
 end
